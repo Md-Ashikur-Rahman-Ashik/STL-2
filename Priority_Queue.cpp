03a8695dc503edc9a6e3d5d84b757d30e9b376a7
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-10-24 11:32:23 +0600

    Couldn't approach Priority Queue problem

---
+++ Priority_Queue.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int numberOfQueue, queries;
+    cin >> numberOfQueue >> queries;
+
+    for (int i = 0; i < numberOfQueue; i++)
+    {
+        string queueName = "priority";
+        queueName.push_back(i);
+
+        
+    }
+    
+    
+
+    return 0;
+}
