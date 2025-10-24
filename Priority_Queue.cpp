3a0176797a0556fa71f0b07855560abff84e5a20
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-10-24 12:08:39 +0600

    Tried building the logic of Priority Queue problem

--- Priority_Queue.cpp
+++ Priority_Queue.cpp
@@ -6,12 +6,14 @@ int main()
     int numberOfQueue, queries;
     cin >> numberOfQueue >> queries;
 
+    priority_queue<priority_queue<int>> nestedQueue;
+
     for (int i = 0; i < numberOfQueue; i++)
     {
         string queueName = "priority";
         queueName.push_back(i);
 
-        
+        priority_queue<int> queueName;
     }
     
     
